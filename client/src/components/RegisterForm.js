7429ee554ebb3358d334d7cb079243f6181cd1a5
tntttom <tntt_tom@me.com>
2020-12-03 23:00:59 -0600

    Account register through form submission

--- client/src/components/RegisterForm.js
+++ client/src/components/RegisterForm.js
@@ -1,29 +1,101 @@
 import React, { useState } from "react";
 import { Link } from "@reach/router";
+import axios from "axios";
 
 export default () => {
+  const [email, setEmail] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [errors, setErrors] = useState({});
+
+  const registerUser = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8000/api/register", {
+        email,
+        firstName,
+        lastName,
+        password,
+      })
+      .then((res) => {
+        if (res.data.errors) {
+          setErrors(res.data.errors);
+          console.log(errors);
+        } else {
+          setEmail("");
+          setFirstName("");
+          setLastName("");
+          setPassword("");
+          setConfirmPassword("");
+        }
+        console.log(res.data);
+      })
+      .catch((err) => console.log(err));
+  };
+
   return (
-    <form className="w-50 mt-4">
+    <form className="w-50 mt-4" onSubmit={registerUser}>
       <h1>Register!</h1>
       <div className="form-group">
-        <label>Email</label>
-        <input className="form-control" type="text" />
+        <label>Email</label>{" "}
+        {errors.email ? (
+          <span className="small text-danger">{errors.email.message}</span>
+        ) : null}
+        <input
+          className="form-control"
+          type="text"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <label>First Name</label>
-        <input className="form-control" type="text" />
+        <label>First Name</label>{" "}
+        {errors.firstName ? (
+          <span className="small text-danger">{errors.firstName.message}</span>
+        ) : null}
+        <input
+          className="form-control"
+          type="text"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <label>Last Name</label>
-        <input className="form-control" type="text" />
+        <label>Last Name</label>{" "}
+        {errors.lastName ? (
+          <span className="small text-danger">{errors.lastName.message}</span>
+        ) : null}
+        <input
+          className="form-control"
+          type="text"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
       </div>
       <div className="form-group">
-        <label>Password</label>
-        <input className="form-control" type="text" autocomplete="false" />
+        <label>Password</label>{" "}
+        {errors.password ? (
+          <span className="small text-danger">{errors.password.message}</span>
+        ) : null}
+        <input
+          className="form-control"
+          type="password"
+          value={password}
+          autocomplete="false"
+          onChange={(e) => setPassword(e.target.value)}
+        />
       </div>
       <div className="form-group mb-0">
         <label>Confirm Password</label>
-        <input className="form-control" type="text" autocomplete="false" />
+        <input
+          className="form-control"
+          type="password"
+          value={confirmPassword}
+          autocomplete="false"
+          onChange={(e) => setConfirmPassword(e.target.value)}
+        />
       </div>
       <Link to="/login">
         <a className="font-weight-light ">Already have an account? Login</a>
