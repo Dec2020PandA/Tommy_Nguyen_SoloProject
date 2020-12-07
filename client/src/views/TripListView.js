8df7124e00e888ff410190c0f5cb167e844f46a6
tntttom <tntt_tom@me.com>
2020-12-07 11:31:00 -0600

    FetchData in TripList - User and Trips Placeholder text for trip list view

--- client/src/views/TripListView.js
+++ client/src/views/TripListView.js
@@ -4,23 +4,37 @@ import axios from "axios";
 
 export default (props) => {
   const [user, setUser] = useState({});
+  const [trips, setTrips] = useState();
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    axios
-      .get(`http://localhost:8000/api/users/${props.id}`)
-      .then((res) => {
-        console.log(res.data);
-        setUser(res.data[0]);
-        setLoaded(true);
-      })
-      .catch((err) => console.log(err));
+    const fetchData = async () => {
+      const userData = await axios.get(
+        `http://localhost:8000/api/users/${props.id}`
+      );
+
+      const tripsData = await axios.get(
+        `http://localhost:8000/api/user/trips/${props.id}`
+      );
+
+      setUser(userData.data[0]);
+
+      setTrips(tripsData.data);
+      setLoaded(true);
+    };
+
+    fetchData();
   }, []);
 
   console.log(user);
+  console.log(trips);
   return (
     <div>
-      <h1>{user.firstName + " " + user.lastName}</h1>
+      {loaded && (
+        <h1>
+          {user.firstName + " " + user.lastName} {trips.length}
+        </h1>
+      )}
     </div>
   );
 };
