a57da5f88f61364d15616ed27a4c84ae889dcfd2
tntttom <tntt_tom@me.com>
2020-12-13 00:54:28 -0600

    Weather API for days + lows and highs

--- client/src/components/WeatherBoard.js
+++ client/src/components/WeatherBoard.js
@@ -1,21 +1,31 @@
 import React, { useState, useEffect } from "react";
-
+import axios from "axios";
 import WeatherCard from "../components/WeatherCard";
 
 import styles from "../css-modules/WeatherBoard.module.css";
 
 export default (props) => {
+  const [weatherDayList, setDayList] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/onecall?lat=41.8781&lon=87.6298&exclude=minutely,hourly&appid=db4ffbf321338ff473960ae65838e81d`
+      )
+      .then((res) => {
+        console.log(res);
+        setDayList(res.data.daily);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+  console.log(weatherDayList);
   return (
     <div className={styles.dashCard}>
       <h6 className={styles.cardLabel}>{props.location}</h6>
-      {/* {weatherList.map((day, index) => (
-        
-      ))} */}
       <div className={styles.weaterList}>
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
-        <WeatherCard />
+        {weatherDayList.slice(0, 4).map((day, index) => (
+          <WeatherCard key={index} index={index} day={day} />
+        ))}
       </div>
     </div>
   );
--- client/src/components/WeatherCard.js
+++ client/src/components/WeatherCard.js
@@ -5,12 +5,15 @@ import styles from "../css-modules/WeatherCard.module.css";
 export default (props) => {
   const [completed, setCompleted] = useState(props.completed);
 
+  console.log(props.day.temp);
   return (
     <div className={styles.weatherCard}>
-      <h1 className={styles.weatherDate}>Date</h1>
+      <h1 className={styles.weatherDate}>Day {props.index + 1}</h1>
       <div className={styles.weatherIcon}>ICON</div>
       <p className={styles.weatherTemps}>
-        <span>50°</span>/<span>72°</span>
+        <span>{Math.floor((props.day.temp.min - 273.15) * (9 / 5) + 32)}°</span>
+        /
+        <span>{Math.floor((props.day.temp.max - 273.15) * (9 / 5) + 32)}°</span>
       </p>
     </div>
   );
